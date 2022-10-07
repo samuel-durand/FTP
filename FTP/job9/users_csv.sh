09b891666fc477b4ad7d8f309d66eb6a1d12b6b6
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2022-10-07 09:14:55 +0200

    script update

--- FTP/job9/users_csv.sh
+++ FTP/job9/users_csv.sh
@@ -1,47 +1,34 @@
-#!/bin/bash
+cd ~/Documents/script/FTP/job9
 
-#cat /home/Documents/script/FTP/job9/users.csv | while read varligne
-#do
- #   password=`echo $varligne |cut -d ',' -f4`
- #   username=`echo $varligne |cut -d ',' -f2`
- #   username=`echo ${username,,}`
- #  role=`echo $varligne |cut -d ',' -f5`
-    
- #   if [ ${role:0:5} = "Admin" ]
- #  then
- #       echo "creation de l'utilisateur : $username"
- #       sudo adduser -m $username -p $password
- #       echo "changement du role de : $username"
- #       sudo usermod -aG sudo $username
- #   else 
- #       echo "creation de l'utilisateur : $username"
- #       sudo adduser -m $username -p $password
- #   fi
-#done < <(tail -n +2 users.csv)
+
+#création du group admin 
 
 sudo groupadd ftpadmin
+sudo chmod 770 ftpadmin
 
-cat ~/Documents/script/FTP/job9/users.csv | while read varligne
+cat users.csv | while read varligne
 do
-    password=`echo $varligne |cut -d ',' -f4`
-    ftpuser=`echo $varligne |cut -d ',' -f2`
-    ftpuser=`echo ${ftpuser,,}`
-    role=`echo $varligne |cut -d ',' -f5`
-    
-    if [ $role = "Admin" ]
-    then
-        echo "creation de l'utilisateur : $ftpuser"
-        pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-	sudo useradd -m -p "$pass" "$ftpuser"
-        
-        echo "changement du role de : $ftpuser"
-        sudo usermod -aG sudo $ftpuser
-        sudo adduser $ftpuser ftpadmin
-    else 
-        echo "creation de l'utilisateur : $ftpuser"
-        pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-	sudo useradd -m -p "$pass" "$ftpuser"
-
-    fi
+	password=`echo $varligne |cut -d ',' -f4`
+	ftpuser=`echo $varligne |cut -d ',' -f2`
+	ftpuser=`echo ${ftpuser,,}`
+	role=`echo $varligne |cut -d ',' -f5`
+	echo $role
+	if [ $role = "Admin" ]
+	then
+		#création user
+		echo "creation de l'utilisateur : $ftpuser"
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		sudo useradd -m -p "$pass" "$ftpuser"
+		#changement de rôle
+		echo "changement du role de : $ftpuser"
+		sudo usermod -aG sudo $ftpuser
+		#changement de groupe
+		echo "changement de groupe"
+		sudo adduser $ftpuser ftpadmin
+	else 
+		#création de groupe
+		echo "creation de l'utilisateur : $ftpuser"
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+    		sudo useradd -m -p "$pass" "$ftpuser"
+	fi
 done < <(tail -n +2 users.csv)
-
